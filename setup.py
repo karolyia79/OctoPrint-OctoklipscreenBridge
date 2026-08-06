6125484fa97bd0fb8f65273a8e86806a38225976
karolyia79 <87782660+karolyia79@users.noreply.github.com>
2026-08-06 15:13:33 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,26 +2,22 @@ from setuptools import setup
 
 plugin_identifier = "octoklipscreen_bridge"
 plugin_package = "octoprint_octoklipscreen_bridge"
-plugin_name = "OctoPrint Octoklipscreen Bridge"
+plugin_name = "OctoPrint-OctoklipscreenBridge"
 plugin_version = "0.1.0"
 plugin_description = "Bridges raw terminal and serial logs to the Octoklipscreen ESP32 display."
 plugin_author = "Andras"
-plugin_url = "https://github.com/karolyia79/OctoPrint-OctoklipscreenBridge"
-plugin_license = "AGPLv3"
-
-plugin_requires = ["paho-mqtt>=1.5.0"]
 
 setup(
     name=plugin_name,
     version=plugin_version,
     description=plugin_description,
     author=plugin_author,
-    url=plugin_url,
     packages=[plugin_package],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=["paho-mqtt>=2.0.0"],
     entry_points="""
         [octoprint.plugin]
-        {} = {}
-    """.format(plugin_identifier, plugin_package),
-    install_requires=plugin_requires,
-    include_package_data=True
+        %s = %s
+    """ % (plugin_identifier, plugin_package)
 )
