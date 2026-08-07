929467d8430d5cbd6e4aba2554321c31abdac8fb
karolyia79 <87782660+karolyia79@users.noreply.github.com>
2026-08-07 17:31:51 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,6 @@
 plugin_identifier = "octoklipscreen_bridge"
 plugin_package = "octoklipscreen_bridge"
 plugin_name = "Octoklipscreen Bridge"
-plugin_version = "0.4.1"# coding=utf-8
-
-plugin_identifier = "octoklipscreen_bridge"
-plugin_package = "octoklipscreen_bridge"
-plugin_name = "Octoklipscreen Bridge"
 plugin_version = "0.4.1"
 plugin_description = "Bridge to send serial logs via MQTT to CYD display"
 plugin_author = "Károlyi András"
@@ -19,11 +14,11 @@ plugin_requires = [
     "paho-mqtt>=1.5.0,<3.0"
 ]
 
-plugin_additional_data = []
+# Itt csatoljuk be a sablonokat, hogy a wheel építés ne szálljon el
+plugin_additional_data = ["templates"]
 plugin_additional_packages = []
 plugin_ignored_packages = []
 
-# Itt kényszerítjük ki a Python 3 kompatibilitást, hogy az OctoPrint ne nyelje be a régi Python 2 korlátot
 additional_setup_parameters = {
     "python_requires": ">=3,<4",
     "classifiers": [
@@ -31,52 +26,6 @@ additional_setup_parameters = {
         "License :: OSI Approved :: GNU Affero General Public License v3 (AGPLv3)",
     ]
 }
-
-from setuptools import setup
-
-try:
-    import octoprint_setuptools
-except:
-    print("Could not import OctoPrint's setuptools, are you sure you are running that under "
-          "the same python installation that OctoPrint is installed under?")
-    import sys
-    sys.exit(-1)
-
-setup_parameters = octoprint_setuptools.create_plugin_setup_parameters(
-    identifier=plugin_identifier,
-    package=plugin_package,
-    name=plugin_name,
-    version=plugin_version,
-    description=plugin_description,
-    author=plugin_author,
-    mail=plugin_author_email,
-    url=plugin_url,
-    license=plugin_license,
-    requires=plugin_requires,
-    additional_packages=plugin_additional_packages,
-    ignored_packages=plugin_ignored_packages,
-    additional_data=plugin_additional_data
-)
-
-if len(additional_setup_parameters):
-    from octoprint.util import dict_merge
-    setup_parameters = dict_merge(setup_parameters, additional_setup_parameters)
-
-setup(**setup_parameters)
-plugin_description = "Bridge to send serial logs via MQTT to CYD display"
-plugin_author = "Károlyi András"
-plugin_author_email = ""
-plugin_url = "https://github.com/karolyia79/OctoklipscreenBridge"
-plugin_license = "AGPLv3"
-
-plugin_requires = [
-    "paho-mqtt>=1.5.0,<3.0"
-]
-
-plugin_additional_data = []
-plugin_additional_packages = []
-plugin_ignored_packages = []
-additional_setup_parameters = {}
 
 from setuptools import setup
 
