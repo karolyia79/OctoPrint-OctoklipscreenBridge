e8a65a1f2f1ae307372a82277b8163124df0a73b
karolyia79 <87782660+karolyia79@users.noreply.github.com>
2026-08-07 07:38:49 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,31 +1,32 @@
+# coding=utf-8
+
 from setuptools import setup
 
-PLUGIN_IDENTIFIER = "octoklipscreen_bridge"
-PLUGIN_PACKAGE = "octoprint_octoklipscreen_bridge"
-PLUGIN_NAME = "OctoPrint-OctoklipscreenBridge"
-PLUGIN_VERSION = "0.1.5"
-PLUGIN_DESCRIPTION = "Bridges OctoPrint terminal to MQTT for CYD displays."
-PLUGIN_AUTHOR = "Andras Karolyi"
-PLUGIN_URL = "https://github.com/karolyia79/OctoPrint-OctoklipscreenBridge"
-PLUGIN_LICENSE = "AGPL-3.0"
+plugin_identifier = "octoklipscreen"
+plugin_package = "octoprint_octoklipscreen"
+plugin_name = "OctoPrint Octoklipscreen Bridge"
+plugin_version = "0.4.0"
+plugin_description = "Bridge to send serial logs via MQTT to CYD display"
+plugin_author = "Károlyi András"
+plugin_license = "AGPLv3"
+
+plugin_requires = [
+    "paho-mqtt>=1.5.0,<2.0"
+]
+
+plugin_additional_data = []
 
 setup(
-    name=PLUGIN_NAME,
-    version=PLUGIN_VERSION,
-    description=PLUGIN_DESCRIPTION,
-    author=PLUGIN_AUTHOR,
-    url=PLUGIN_URL,
-    license=PLUGIN_LICENSE,
-    packages=[PLUGIN_PACKAGE],
-    package_data={
-        PLUGIN_PACKAGE: ["templates/*.jinja2"],
-    },
+    name=plugin_name,
+    version=plugin_version,
+    description=plugin_description,
+    author=plugin_author,
+    license=plugin_license,
+    packages=[plugin_package],
     include_package_data=True,
-    zip_safe=False,
-    python_requires=">=3,<4",
-    install_requires=[],
+    install_requires=plugin_requires,
     entry_points="""
-        [octoprint.plugin]
-        {0} = {1}
-    """.format(PLUGIN_IDENTIFIER, PLUGIN_PACKAGE)
+    [octoprint.plugin]
+    {identifier} = {package}
+    """.format(identifier=plugin_identifier, package=plugin_package)
 )
