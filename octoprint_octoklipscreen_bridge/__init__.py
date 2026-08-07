a97c369daf1aa893487b1f9c275ddbb041447d9c
karolyia79 <87782660+karolyia79@users.noreply.github.com>
2026-08-07 06:55:48 +0200

    Update __init__.py

--- octoprint_octoklipscreen_bridge/__init__.py
+++ octoprint_octoklipscreen_bridge/__init__.py
@@ -2,12 +2,13 @@
 from __future__ import absolute_import
 
 __plugin_name__ = "Octoklipscreen Bridge"
-__plugin_version__ = "0.2.0"
-__plugin_description__ = "Bridges OctoPrint full terminal to MQTT for CYD displays."
+__plugin_version__ = "0.2.1"
+__plugin_description__ = "Bridges OctoPrint log/terminal to MQTT for CYD displays."
 __plugin_pythoncompat__ = ">=3,<4"
 
 import octoprint.plugin
 import paho.mqtt.client as mqtt
+import logging
 
 class OctoklipscreenBridgePlugin(octoprint.plugin.StartupPlugin,
                                   octoprint.plugin.SettingsPlugin,
@@ -24,7 +25,6 @@ class OctoklipscreenBridgePlugin(octoprint.plugin.StartupPlugin,
         mqtt_user = self._settings.get(["mqtt_user"])
         mqtt_pass = self._settings.get(["mqtt_pass"])
         
-        self._logger.info("Initializing MQTT broker connection to: {}".format(broker_ip))
         try:
             try:
                 self.mqtt_client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION1, "OctoPrint_Bridge")
@@ -36,31 +36,17 @@ class OctoklipscreenBridgePlugin(octoprint.plugin.StartupPlugin,
             
             self.mqtt_client.connect(broker_ip, 1883, 60)
             self.mqtt_client.loop_start()
-            self._logger.info("Successfully connected to MQTT at {}".format(broker_ip))
+            self._logger.info("Connected to MQTT at {}".format(broker_ip))
         except Exception as e:
             self._logger.error("MQTT Connection Error: {}".format(e))
 
-    # Elkapja azt, amit az OctoPrint küld a nyomtatónak
-    def on_sending_line(self, comm_instance, phase, cmd, parameters, *args, **kwargs):
-        if cmd:
-            full_cmd = "{} {}".format(cmd, parameters).strip() if parameters else cmd
-            self._publish_to_mqtt(">> " + full_cmd)
-        elif parameters:
-            self._publish_to_mqtt(">> " + parameters)
-        return None
-
-    # Elkapja azt, amit a nyomtatómotor visszaküld
-    def on_received_line(self, comm_instance, line, *args, **kwargs):
-        if line:
-            self._publish_to_mqtt("<< " + line.strip())
-        return line
-
-    def _publish_to_mqtt(self, text):
-        if self.mqtt_client:
+    def publish_log(self, log_entry):
+        if self.mqtt_client and log_entry:
             try:
-                self.mqtt_client.publish("octoklipscreen/terminal", text)
-            except Exception as e:
-                self._logger.error("MQTT Publish Error: {}".format(e))
+                # Elküldjük a log üzenetet az MQTT-re
+                self.mqtt_client.publish("octoklipscreen/terminal", str(log_entry))
+            except Exception:
+                pass
 
     def get_settings_defaults(self):
         return dict(mqtt_broker="localhost", mqtt_user="mosquitto", mqtt_pass="")
@@ -68,12 +54,29 @@ class OctoklipscreenBridgePlugin(octoprint.plugin.StartupPlugin,
     def get_template_configs(self):
         return [dict(type="settings", custom_bindings=False, template="octoklipscreen_bridge_settings.jinja2")]
 
+# Létrehozunk egy egyedi logging handler-t, ami rákötődik az OctoPrint kommunikációs logjára
+class MQTTLogHandler(logging.Handler):
+    def __init__(self, plugin_instance):
+        logging.Handler.__init__(self)
+        self.plugin_instance = plugin_instance
+
+    def emit(self, record):
+        try:
+            msg = self.format(record)
+            # Ha a kommunikációs logból jön a sor, továbbítjuk
+            if "octoprint.communication" in record.name:
+                self.plugin_instance.publish_log(msg)
+        except Exception:
+            pass
+
 def __plugin_load__():
     global __plugin_implementation__
     __plugin_implementation__ = OctoklipscreenBridgePlugin()
 
+    # Rákötjük a handlert az OctoPrint loggolási rendszerére
+    root_logger = logging.getLogger("octoprint.communication")
+    handler = MQTTLogHandler(__plugin_implementation__)
+    root_logger.addHandler(handler)
+
     global __plugin_hooks__
-    __plugin_hooks__ = {
-        "octoprint.comm.protocol.sending": __plugin_implementation__.on_sending_line,
-        "octoprint.comm.protocol.received": __plugin_implementation__.on_received_line
-    }
+    __plugin_hooks__ = {}
