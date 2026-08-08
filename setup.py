743eb7dfedec72f0d88f44dda8865763738fb4e7
karolyia79 <87782660+karolyia79@users.noreply.github.com>
2026-08-08 16:45:43 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,121 +1,92 @@
 # coding=utf-8
 
-"""
-OctoPrint plugin setup configuration with improved compatibility and error handling.
-Requires Python 3.7+ and OctoPrint 1.4.0+
-"""
+########################################################################################################################
+### Do not forget to adjust the following variables to your own plugin.
 
-import sys
-import os
-
-# Verzió-ellenőrzés
-if sys.version_info[0] < 3 or (sys.version_info[0] == 3 and sys.version_info[1] < 7):
-    sys.exit(
-        "ERROR: OctoklipscreenBridge requires Python 3.7 or higher.\n"
-        f"You are using Python {sys.version_info[0]}.{sys.version_info[1]}.\n"
-        "Please activate the OctoPrint virtual environment:\n"
-        "  source ~/oprint/bin/activate"
-    )
-
-# Plugin metadata
+# The plugin's identifier, has to be unique
 plugin_identifier = "octoklipscreen_bridge"
+
+# The plugin's python package, should be "octoprint_<plugin identifier>", has to be unique
 plugin_package = "octoklipscreen_bridge"
+
+# The plugin's human readable name. Can be overwritten within OctoPrint's internal data via __plugin_name__ in the
+# plugin module
 plugin_name = "Octoklipscreen Bridge"
+
+# The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
 plugin_version = "0.4.3"
+
+# The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
+# module
 plugin_description = "Bridge to send serial logs via MQTT to CYD display"
+
+# The plugin's author. Can be overwritten within OctoPrint's internal data via __plugin_author__ in the plugin module
 plugin_author = "Károlyi András"
+
+# The plugin's author's mail address.
 plugin_author_email = ""
+
+# The plugin's homepage URL. Can be overwritten within OctoPrint's internal data via __plugin_url__ in the plugin module
 plugin_url = "https://github.com/karolyia79/OctoklipscreenBridge"
+
+# The plugin's license. Can be overwritten within OctoPrint's internal data via __plugin_license__ in the plugin module
 plugin_license = "AGPLv3"
 
-# Függőségek
+# Any additional requirements besides OctoPrint should be listed here
 plugin_requires = [
     "paho-mqtt>=1.5.0,<3.0"
 ]
 
-# Adatok és csomagok
+### --------------------------------------------------------------------------------------------------------------------
+### More advanced options that you usually shouldn't have to touch follow after this point
+### --------------------------------------------------------------------------------------------------------------------
+
+# Additional package data to install for this plugin. The subfolders "templates", "static" and "translations" will
+# already be installed automatically if they exist.
 plugin_additional_data = ["templates"]
-plugin_additional_packages = []
+
+# Any additional python packages you need to install with your plugin that are not contains in <plugin_package>.*
+plugin_addtional_packages = []
+
+# Any python packages within <plugin_package>.* you do NOT want to install with your plugin
 plugin_ignored_packages = []
 
-# Extra setup paraméterek
-additional_setup_parameters = {
-    "python_requires": ">=3,<4",
-    "classifiers": [
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-        "License :: OSI Approved :: GNU Affero General Public License v3 (AGPLv3)",
-        "Operating System :: OS Independent",
-        "Topic :: System :: Hardware :: Hardware Drivers",
-    ]
-}
+# Additional parameters for the call to setuptools.setup. If your plugin wants to register additional entry points,
+# define dependency links or other things like that, this is the place to go. Will be merged recursively with the
+# default setup parameters as provided by octoprint_setuptools.create_plugin_setup_parameters using
+# octoprint.util.dict_merge.
+additional_setup_parameters = {}
 
-# OctoPrint setuptools import (biztonságos fallback-kel az izolált webes telepítőkhöz)
-def import_octoprint_setuptools():
-    """
-    Import OctoPrint setuptools with informative error handling.
-    """
-    try:
-        import octoprint_setuptools
-        return octoprint_setuptools
-    except ImportError:
-        return None
+########################################################################################################################
 
-# Import és konfigurálás
-octoprint_setuptools = import_octoprint_setuptools()
+from setuptools import setup
 
 try:
-    from setuptools import setup
-except ImportError:
-    print("ERROR: setuptools is not installed!")
-    print("Please install setuptools: pip install --upgrade setuptools")
-    sys.exit(1)
+	import octoprint_setuptools
+except:
+	print("Could not import OctoPrint's setuptools, are you sure you are running that under "
+	      "the same python installation that OctoPrint is installed under?")
+	import sys
+	sys.exit(-1)
 
-# Setup paraméterek összeállítása
-if octoprint_setuptools is not None:
-    setup_parameters = octoprint_setuptools.create_plugin_setup_parameters(
-        identifier=plugin_identifier,
-        package=plugin_package,
-        name=plugin_name,
-        version=plugin_version,
-        description=plugin_description,
-        author=plugin_author,
-        mail=plugin_author_email,
-        url=plugin_url,
-        license=plugin_license,
-        requires=plugin_requires,
-        additional_packages=plugin_additional_packages,
-        ignored_packages=plugin_ignored_packages,
-        additional_data=plugin_additional_data
-    )
-else:
-    # Fallback, ha az OctoPrint izolált környezetben futtatja a webes telepítőt
-    setup_parameters = dict(
-        name=plugin_name,
-        version=plugin_version,
-        description=plugin_description,
-        author=plugin_author,
-        author_email=plugin_author_email,
-        url=plugin_url,
-        license=plugin_license,
-        packages=[plugin_package],
-        include_package_data=True,
-        install_requires=plugin_requires,
-        zip_safe=False
-    )
+setup_parameters = octoprint_setuptools.create_plugin_setup_parameters(
+	identifier=plugin_identifier,
+	package=plugin_package,
+	name=plugin_name,
+	version=plugin_version,
+	description=plugin_description,
+	author=plugin_author,
+	mail=plugin_author_email,
+	url=plugin_url,
+	license=plugin_license,
+	requires=plugin_requires,
+	additional_packages=plugin_addtional_packages,
+	ignored_packages=plugin_ignored_packages,
+	additional_data=plugin_additional_data
+)
 
-# Extra paraméterek hozzáadása
-if additional_setup_parameters:
-    try:
-        from octoprint.util import dict_merge
-        setup_parameters = dict_merge(setup_parameters, additional_setup_parameters)
-    except ImportError:
-        setup_parameters.update(additional_setup_parameters)
+if len(additional_setup_parameters):
+	from octoprint.util import dict_merge
+	setup_parameters = dict_merge(setup_parameters, additional_setup_parameters)
 
-# Setup futtatása
-if __name__ == "__main__":
-    setup(**setup_parameters)
+setup(**setup_parameters)
